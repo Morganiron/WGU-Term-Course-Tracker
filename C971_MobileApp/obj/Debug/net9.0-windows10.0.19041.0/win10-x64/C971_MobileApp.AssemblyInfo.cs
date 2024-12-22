6754230a38de47692d80384eff1f21ed398440dc
Morganiron <86537433+Morganiron@users.noreply.github.com>
2024-12-21 19:12:01 -0600

    Task C1 - Added page to add or edit a term, updated Color and Style resources

--- C971_MobileApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/C971_MobileApp.AssemblyInfo.cs
+++ C971_MobileApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/C971_MobileApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7fa3b7bdadeb1ce8477d10acc5c52ea9d061e55f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+822c0b7c9d85a03993bbf2c04a689f08a7fd3af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
