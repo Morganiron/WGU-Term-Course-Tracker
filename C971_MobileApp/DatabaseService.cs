7fa3731e4817eb1b68a8367a862bdcd9889e0f41
Morganiron <86537433+Morganiron@users.noreply.github.com>
2025-01-06 14:49:27 -0600

    Task C2 - created database service to save term and class information

---
+++ C971_MobileApp/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C971_MobileApp
+{
+    internal static class DatabaseService
+    {
+        private static SQLiteAsyncConnection _database;
+
+        public static async Task InitializeAsync()
+        {
+            if (_database != null) return;
+
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "C971.db");
+            _database = new SQLiteAsyncConnection(databasePath);
+
+            await _database.CreateTableAsync<Term>();
+            await _database.CreateTableAsync<Course>();
+        }
+
+        public static SQLiteAsyncConnection Database => _database;
+    }
+}
