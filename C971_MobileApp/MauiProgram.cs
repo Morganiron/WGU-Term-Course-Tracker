fd8490a7ed672a015642d7d64ede34c2b1937bf5
Morganiron <86537433+Morganiron@users.noreply.github.com>
2025-01-09 13:17:52 -0600

    Update MaterialIcons font name

--- C971_MobileApp/MauiProgram.cs
+++ C971_MobileApp/MauiProgram.cs
@@ -18,7 +18,7 @@ namespace C971_MobileApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("material_icon_regular.ttf", "MaterialIcon");
+                    fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcon");
                 }); 
 
 #if DEBUG
