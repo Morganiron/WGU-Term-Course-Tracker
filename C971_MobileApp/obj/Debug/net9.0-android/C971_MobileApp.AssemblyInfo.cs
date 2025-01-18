dbbf7d345f88dd98e1b7d6d7fdb447d3f37ebaf5
Morganiron <86537433+Morganiron@users.noreply.github.com>
2025-01-18 15:58:11 -0600

    Task C4 - makes changes to project and emulator to properly handle notifications

--- C971_MobileApp/obj/Debug/net9.0-android/C971_MobileApp.AssemblyInfo.cs
+++ C971_MobileApp/obj/Debug/net9.0-android/C971_MobileApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6e57944290458b88ae58bde1eded530a27437d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+711b2614d8d39ce5a332503204efdc298958ca4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C971_MobileApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/C971_MobileApp.AssemblyInfo.cs
+++ C971_MobileApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/C971_MobileApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+23ab0507154c9d89ca1b4ed89bdc0b53b1f92f77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+711b2614d8d39ce5a332503204efdc298958ca4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C971_MobileApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
